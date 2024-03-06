ae2e31d112b609ad70a2f97c120eba37fa5a7d16
Julie <julwturner@gmail.com>
2024-03-06 15:15:19 -0700

    updating for zoomed in fires for dehcho and sahtu

--- scripts/3-animation.R
+++ scripts/3-animation.R
@@ -15,14 +15,13 @@ require(amt)
 require(ggplot2)
 require(ggmap)
 require(ggspatial)
-#require(basemaps)
+require(basemaps)
 require(gganimate)
 require(gifski)
 
 ### Input data ----
 raw <- file.path('data', 'raw')
 derived <- file.path('data', 'derived')
-canada <- file.path('~/Dropbox', 'ActiveDocs', 'Git-local', 'Can_GIS_layers')
 
 cbou <- readRDS(file.path(raw, 'NWTdat.RDS'))
 
@@ -31,15 +30,12 @@ hotspots <- readRDS(file.path(raw, 'hotspots2023_nwt.RDS'))
 progression <- st_read(file.path(raw, 'progression_nwt.shp'))
 burns <- st_read(file.path(raw, 'FireShapefiles', 'brnGEE_2023_Merge.shp'))
 
-canPoly <- st_read(file.path(canada, 'CanadaPoly', 'lpr_000b16a_e.shp'))
 
 # prep ----
 # pseudo mercator
 crs.web <- st_crs(3857)$wkt
 crs <- st_crs(4326)$wkt
 
-nwt <- filter(canPoly, PREABBR %in% c('N.W.T.'))
-nwt.wgs <- st_transform(nwt, crs)
 
 ## fires ----
 # 15km buffered enterprise fire
@@ -48,6 +44,20 @@ enterprise.f <- burns %>%
   st_buffer(dist = 15000) %>%
   st_transform(crs = crs) 
 plot(enterprise.f$geometry)
+
+# 15km buffered dehcho zoom fire
+dehchozoom.f <- burns %>%
+  filter(FIRENUM == 11345439) %>%
+  st_buffer(dist = 15000) %>%
+  st_transform(crs = crs) 
+plot(dehchozoom.f$geometry)
+
+# 15km buffered sahtu zoom fire
+sahtuzoom.f <- burns %>%
+  filter(FIRENUM == 11346855) %>%
+  st_buffer(dist = 15000) %>%
+  st_transform(crs = crs) 
+plot(sahtuzoom.f$geometry)
 
 ## caribou ----
 fixrate <- cbou %>% make_track(x,y, timestamp, crs = crs, all_cols = T) %>% 
@@ -74,25 +84,67 @@ s.slave <- cbou.MayOct[study_area == 'south.slave']
 
 
 cbou.coords <- st_as_sf(cbou.MayOct, coords = c('x', 'y')) %>%
-  st_set_crs(4326)
+  st_set_crs(4326) 
+
+cbou.coords.df <- setDT(cbou.coords %>%
+  st_transform(crs.web) %>%
+  sfheaders::sf_to_df(fill = T))
+
+
 dehcho.coords <- st_as_sf(dehcho, coords = c('x', 'y')) %>%
   st_set_crs(4326)
-sahtu.coords <- st_as_sf(sahtu, coords = c('x', 'y')) %>%
-  st_set_crs(4326)
-s.slave.coords <- st_as_sf(s.slave, coords = c('x', 'y')) %>%
-  st_set_crs(4326)
 
-# gps points within 15km of the enterprise fire
+dehcho.df <- setDT(dehcho.coords %>%
+                          st_transform(crs.web) %>%
+                          sfheaders::sf_to_df(fill = T))
+
+
+sahtu.coords <- st_as_sf(sahtu, coords = c('x', 'y')) %>%
+  st_set_crs(4326) 
+
+sahtu.df <- setDT(sahtu.coords %>% 
+                   st_transform(crs.web) %>%
+                     sfheaders::sf_to_df(fill = T))
+
+s.slave.coords <- st_as_sf(s.slave, coords = c('x', 'y')) %>%
+  st_set_crs(4326) 
+
+sslave.df <- setDT(s.slave.coords %>%
+                     st_transform(crs.web) %>%
+                     sfheaders::sf_to_df(fill = T))
+
+# gps points within 15km of the zoomed in fire
 enterprise <- cbou.coords %>%
   st_join(enterprise.f, join = st_within) %>%
   filter(!is.na(FIRENUM))
 enterprise.df <- setDT(sfheaders::sf_to_df(enterprise, fill = T))
+enterprise.df.web <- setDT(enterprise %>%
+                     st_transform(crs.web) %>%
+                     sfheaders::sf_to_df(fill = T))
+
+dehchozoom <- cbou.coords %>%
+  st_join(dehchozoom.f, join = st_within) %>%
+  filter(!is.na(FIRENUM))
+dehchozoom.df <- setDT(sfheaders::sf_to_df(dehchozoom, fill = T))
+dehchozoom.df.web <- setDT(dehchozoom %>%
+                             st_transform(crs.web) %>%
+                             sfheaders::sf_to_df(fill = T))
+
+sahtuzoom <- cbou.coords %>%
+  st_join(sahtuzoom.f, join = st_within) %>%
+  filter(!is.na(FIRENUM))
+sahtuzoom.df <- setDT(sfheaders::sf_to_df(sahtuzoom, fill = T))
+sahtuzoom.df.web <- setDT(sahtuzoom %>%
+                             st_transform(crs.web) %>%
+                             sfheaders::sf_to_df(fill = T))
 
 cbou.ext <- st_bbox(cbou.coords)
 dehcho.ext <- st_bbox(dehcho.coords)
 sahtu.ext <- st_bbox(sahtu.coords)
 s.slave.ext <- st_bbox(s.slave.coords)
 enterprise.ext <- st_bbox(enterprise.f)
+dehchozoom.ext <- st_bbox(dehchozoom.f)
+sahtuzoom.ext <- st_bbox(sahtuzoom.f)
 
 ## fires to bou area ----
 hotspots.sub <- hotspots[datetime < as.POSIXct('2023-10-01', tz='UTC')]
@@ -119,6 +171,15 @@ hotspots.enterprise <- st_as_sf(hotspots.sub, coords = c('x', 'y')) %>%
   st_set_crs(4326) %>%
   st_crop(enterprise.ext)
 
+hotspots.dehchozoom <- st_as_sf(hotspots.sub, coords = c('x', 'y')) %>%
+  st_set_crs(4326) %>%
+  st_crop(dehchozoom.ext)
+
+hotspots.sahtuzoom <- st_as_sf(hotspots.sub, coords = c('x', 'y')) %>%
+  st_set_crs(4326) %>%
+  st_crop(sahtuzoom.ext)
+
+
 
 
 hotspots.df <- setDT(sfheaders::sf_to_df(hotspots.bou, fill = T))
@@ -126,15 +187,17 @@ hotspots.dehcho.df <- setDT(sfheaders::sf_to_df(hotspots.dehcho, fill = T))
 hotspots.sahtu.df <- setDT(sfheaders::sf_to_df(hotspots.sahtu, fill = T))
 hotspots.sslave.df <- setDT(sfheaders::sf_to_df(hotspots.sslave, fill = T))
 hotspots.enterprise.df <- setDT(sfheaders::sf_to_df(hotspots.enterprise, fill = T))
+hotspots.dehchozoom.df <- setDT(sfheaders::sf_to_df(hotspots.dehchozoom, fill = T))
+hotspots.sahtuzoom.df <- setDT(sfheaders::sf_to_df(hotspots.sahtuzoom, fill = T))
 
-
-# Don't neeed these currently
+# Don't need these currently
 progression.sub <- filter(progression, datetime < as.POSIXct('2023-10-01', tz='UTC'))
 progression.sub$datetime <- as.POSIXct(paste0(as.character(progression.sub$DATE), ' 23:59:59'), tz = 'UTC',
                                       format ='%Y%m%d %H:%M:%OS')
 progression.sub$tod <- lubridate::ceiling_date(progression.sub$datetime, "8 hours")
 
 progression.bou <- progression.sub %>%
+  st_transform(crs.web) %>%
   st_crop(cbou.ext)
 #plot(progression.bou$geometry)
 
@@ -157,21 +220,27 @@ names(dehcho.ext) <- c('left', 'bottom', 'right', 'top')
 names(sahtu.ext) <- c('left', 'bottom', 'right', 'top')
 names(s.slave.ext) <- c('left', 'bottom', 'right', 'top')
 names(enterprise.ext) <- c('left', 'bottom', 'right', 'top')
+names(dehchozoom.ext) <- c('left', 'bottom', 'right', 'top')
+names(sahtuzoom.ext) <- c('left', 'bottom', 'right', 'top')
 
 # setting defaults outside of this b/c need an API from stadiamaps.com
 ## register_stadiamaps(key = "xxxxx")
 
 # nwtmap <- get_map(location = myloc, source = 'stadia', maptype = 'stamen_toner_lite', 
 #                   crop = FALSE, zoom = 6)
-dehchomap <- get_map(location = dehcho.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
+dehchomap <- get_map(location = dehcho.ext, source = 'stadia', maptype = 'stamen_terrain_background', 
                   crop = FALSE, zoom = 8)
-sahtumap <- get_map(location = sahtu.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
+sahtumap <- get_map(location = sahtu.ext, source = 'stadia', maptype = 'stamen_terrain_background', 
                      crop = FALSE, zoom = 8)
-sslavemap <- get_map(location = s.slave.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
+sslavemap <- get_map(location = s.slave.ext, source = 'stadia', maptype = 'stamen_terrain_background', 
                      crop = FALSE, zoom = 9)
 
 enterprisemap <- get_map(location = enterprise.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
                     crop = FALSE, zoom = 10)
+dehchozoommap <- get_map(location = dehchozoom.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
+                         crop = FALSE, zoom = 10)
+sahtuzoommap <- get_map(location = sahtuzoom.ext, source = 'stadia', maptype = 'stamen_toner_lite', 
+                         crop = FALSE, zoom = 10)
 
 
 ### whole NWT ----
@@ -206,12 +275,31 @@ p.dehcho <- ggmap(dehchomap) +
              size = 2.25, show.legend = F) +
   annotation_scale(location = 'tl', width_hint = 0.3) +
   coord_sf(crs = crs) + 
-  scale_color_viridis_d() 
+  scale_color_viridis_d(option = 'mako') 
 
+dehcho.web <- dehcho.coords %>% st_transform(crs.web)
+hotspots.dehcho.web <- setDT(hotspots.dehcho %>% 
+                               st_transform(crs.web) %>%
+                               sfheaders::sf_to_df(fill = T))
+basemap_ggplot(ext = dehcho.web, map_service = 'osm', map_type = 'topographic')
+# 
+p.dehcho <-
+ # ggplot() + #annotation_map_tile() +
+  basemap_ggplot(ext = dehcho.web, map_service = 'osm', map_type = 'topographic') +
+  #basemap_gglayer(ext = dehcho.web, map_service = 'osm', map_type = 'topographic') +
+  geom_point(data = hotspots.dehcho.web, aes(x=x, y=y, group = seq_along(tod)),
+             shape = 17, color = 'darkorange', show.legend = F) +
+  geom_point(data = dehcho.df, aes(x=x, y=y, group = id, color = id),
+             size = 2.25, show.legend = F) +
+  annotation_scale(location = 'tl', width_hint = 0.3) +
+  theme_bw() +
+  #coord_sf(crs = crs) +
+  scale_color_viridis_d(option = 'mako')
+p.dehcho
 
 p.dehcho.anim <- p.dehcho +
   transition_time(tod) +
-  shadow_mark(color = 'maroon', alpha = 0.25, exclude_layer = 5) +
+  shadow_mark(color = 'maroon', alpha = 0.25, exclude_layer = 3) + # 5 for ggmap
   exit_shrink() +
   ease_aes('linear') +
   labs(title="{frame_time}", x="Longitude", y="Latitude")
@@ -221,6 +309,34 @@ animate(p.dehcho.anim, nframes = num_frames, fps = 2)
 anim_save(file.path('anims', "dehcho.gif"))
 animate(p.dehcho.anim, nframes = num_frames, fps = 2, renderer = ffmpeg_renderer())
 anim_save(file.path('anims', "dehcho.mp4"))
+
+#### dehcho zoom ----
+hotspots.dehchozoom.Aug <- hotspots.dehchozoom.df[datetime >= as.POSIXct('2023-07-01', tz='UTC')]
+dehchozoom.Aug <- dehchozoom.df[datetime>= as.POSIXct('2023-08-01', tz='UTC')]
+
+p.dehchozoom <- ggmap(dehchozoommap) +
+  geom_point(data = hotspots.dehchozoom.Aug, aes(x=x, y=y, group = seq_along(tod)), 
+             shape = 17, color = 'darkorange', show.legend = F) +
+  geom_point(data = dehchozoom.Aug, aes(x=x, y=y, group = id, color = id), 
+             size = 2.25, show.legend = F) +
+  annotation_scale(location = 'tl', width_hint = 0.3) +
+  coord_sf(crs = crs) + 
+  scale_color_viridis_d() 
+
+
+p.dehchozoom.anim <- p.dehchozoom +
+  transition_time(tod) +
+  shadow_mark(color = 'maroon', alpha = 0.25, exclude_layer = 5) +
+  exit_shrink() +
+  ease_aes('linear') +
+  labs(title="{frame_time}", x="Longitude", y="Latitude")
+
+num_frames <- length(unique(dehchozoom.Aug$tod))
+animate(p.dehchozoom.anim, nframes = num_frames, fps = 2)
+anim_save(file.path('anims', "dehchozoom.gif"))
+animate(p.dehchozoom.anim, nframes = num_frames, fps = 2, renderer = ffmpeg_renderer())
+anim_save(file.path('anims', "dehchozoom.mp4"))
+
 
 
 ### Sahtú ----
@@ -247,7 +363,34 @@ anim_save(file.path('anims', "sahtu.gif"))
 animate(p.sahtu.anim, nframes = num_frames, fps = 2, renderer = ffmpeg_renderer())
 anim_save(file.path('anims', "sahtu.mp4"))
 
+#### dehcho zoom ----
+hotspots.sahtuzoom.Jul <- hotspots.sahtuzoom.df[datetime %between% c(as.POSIXct('2023-07-01', tz='UTC'),
+                                                as.POSIXct('2023-08-01', tz='UTC'))]
+sahtuzoom.Jul <- sahtuzoom.df[datetime %between% c(as.POSIXct('2023-07-01', tz='UTC'),
+                                                   as.POSIXct('2023-08-01', tz='UTC'))]
 
+p.sahtuzoom <- ggmap(sahtuzoommap) +
+  geom_point(data = hotspots.sahtuzoom.Jul, aes(x=x, y=y, group = seq_along(tod)), 
+             shape = 17, color = 'darkorange', show.legend = F) +
+  geom_point(data = sahtuzoom.Jul, aes(x=x, y=y, group = id, color = id), 
+             size = 2.25, show.legend = F) +
+  annotation_scale(location = 'tl', width_hint = 0.3) +
+  coord_sf(crs = crs) + 
+  scale_color_viridis_d() 
+
+
+p.sahtuzoom.anim <- p.sahtuzoom +
+  transition_time(tod) +
+  shadow_mark(color = 'maroon', alpha = 0.25, exclude_layer = 5) +
+  exit_shrink() +
+  ease_aes('linear') +
+  labs(title="{frame_time}", x="Longitude", y="Latitude")
+
+num_frames <- length(unique(sahtuzoom.Jul$tod))
+animate(p.sahtuzoom.anim, nframes = num_frames, fps = 2)
+anim_save(file.path('anims', "sahtuzoom.gif"))
+animate(p.sahtuzoom.anim, nframes = num_frames, fps = 2, renderer = ffmpeg_renderer())
+anim_save(file.path('anims', "sahtuzoom.mp4"))
 
 ### South Slave ----
 p.sslave <- ggmap(sslavemap) +
@@ -304,7 +447,6 @@ animate(p.enterprise.anim, nframes = num_frames, fps = 2)
 anim_save(file.path('anims', "enterprise.gif"))
 animate(p.enterprise.anim, nframes = num_frames, fps = 2, renderer = ffmpeg_renderer())
 anim_save(file.path('anims', "enterprise.mp4"))
-
 
 
 #####
